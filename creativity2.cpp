b212a38a7c1373dc1148054f5ec3f81f63d96b1f
syedfaisalilyas <120155473+syedfaisalilyas@users.noreply.github.com>
2022-12-14 11:55:10 +0500

    PF Week02 Lab

---
+++ creativity2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+main(){
+cout << "            _______________                    " <<endl;
+cout << "           |               |                   " <<endl; 
+cout << "           |   +       +   |                   " <<endl; 
+cout << "           |       -       |                   " <<endl; 
+cout << "           |_______________|                   " <<endl; 
+cout << "            ______| |______                    " <<endl; 
+cout << "           |               |                   " <<endl; 
+cout << "          /|               |\\                  " <<endl; 
+cout << "         / |               | \\                 " <<endl; 
+cout << "        /  |               |  \\                " <<endl; 
+cout << "       -   |_______________|   -                " <<endl; 
+cout << "               |      |                        " <<endl; 
+cout << "               |      |                        " <<endl; 
+cout << "               |      |                        " <<endl; 
+cout << "               -      -                        " <<endl;  
+
+
+}
